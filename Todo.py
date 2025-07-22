48ead85f1bfd8a489b98ef204d98caa4c2cba2a4
Taylor Putman <fishputman@gmail.com>
2025-07-22 16:19:10 -0400

    add \n for spacing on inputs

--- Todo.py
+++ Todo.py
@@ -6,37 +6,38 @@ def showMenu():
     print("1. Add Task")
     print("2. View Tasks")
     print("3. Delete Task")
-    print("4. Quit")
+    print("4. Quit \n")
 
 
 while True:
     showMenu()
-    task = input("What would you like to do? ").strip().lower()
+    task = input("What would you like to do? \n").strip().lower()
 
     if task == "add task" or task == "1":
-        addTask = input("What task do you want to add? ")
+        addTask = input("What task do you want to add? \n")
         todoList.append(addTask)
-        print(f"You added {addTask} to your todo list.")
+        print(f"You added {addTask} to your todo list.\n")
 
     elif task == "view tasks" or task == "2":
         if len(todoList) == 0:
-            print("Your todo list is Empty!")
+            print("Your todo list is Empty!\n")
         else:
             for index, task in enumerate(todoList):
-                print(f"{index + 1}: {task}")
+                print("Your tasks include: ")
+                print(f"{index + 1}: {task}\n")
 
     elif task == "delete task" or task == "3":
         try:
-            deleteTask = int(input("Enter the number of the task to delete: "))
-            print(f"You removed {todoList[deleteTask - 1]} from your todo list.")
+            deleteTask = int(input("Enter the number of the task to delete: \n"))
+            print(f"You removed {todoList[deleteTask - 1]} from your todo list.\n")
             todoList.pop(deleteTask - 1)
         except ValueError:
-            print("Please enter a valid number.")
+            print("Please enter a valid number.\n")
         except IndexError:
-            print("Task does not exist.")
+            print("Task does not exist.\n")
 
     elif task == "quit" or task == "4":
         break
 
     else:
-        print("Invalid option, please try again.")
+        print("Invalid option, please try again.\n")
