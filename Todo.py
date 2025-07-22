0f8e48719ed4c835e505b363f38c38408c008e1b
Taylor Putman <fishputman@gmail.com>
2025-07-22 16:33:00 -0400

    added logic to check for no input entered

--- Todo.py
+++ Todo.py
@@ -12,29 +12,44 @@ def showMenu():
 while True:
     showMenu()
     task = input("What would you like to do? \n").strip().lower()
+    if not task:
+        print("You must choose an option.\n")
+        continue
 
     if task == "add task" or task == "1":
-        addTask = input("What task do you want to add? \n")
-        todoList.append(addTask)
-        print(f"You added {addTask} to your todo list.\n")
+        addTask = input("What task do you want to add? \n").strip()
+        if not addTask:
+            print("You cannot add a blank task.\n")
+        else:
+            todoList.append(addTask)
+            print(f"You added {addTask} to your todo list.\n")
 
     elif task == "view tasks" or task == "2":
         if len(todoList) == 0:
             print("Your todo list is Empty!\n")
         else:
+            print("Your tasks include: ")
             for index, task in enumerate(todoList):
-                print("Your tasks include: ")
                 print(f"{index + 1}: {task}\n")
 
     elif task == "delete task" or task == "3":
-        try:
-            deleteTask = int(input("Enter the number of the task to delete: \n"))
-            print(f"You removed {todoList[deleteTask - 1]} from your todo list.\n")
-            todoList.pop(deleteTask - 1)
-        except ValueError:
-            print("Please enter a valid number.\n")
-        except IndexError:
-            print("Task does not exist.\n")
+        if len(todoList) == 0:
+            print("Your todo list is Empty!\n")
+        else:
+            deleteInput = input("Enter the number of the task to delete: \n").strip()
+            if not deleteInput:
+                print("You must enter a task number.\n")
+            else:
+                try:
+                    deleteTask = int(deleteInput)
+                    print(
+                        f"You removed {todoList[deleteTask - 1]} from your todo list.\n"
+                    )
+                    todoList.pop(deleteTask - 1)
+                except ValueError:
+                    print("Please enter a valid number.\n")
+                except IndexError:
+                    print("Task does not exist.\n")
 
     elif task == "quit" or task == "4":
         break
